c8cfe159d063ceb61352058dbea22431625539e6
I am Roshik <Roshik9841@gmail.com>
2026-05-11 18:41:59 +0545

    Page title

--- playwright.config.js
+++ playwright.config.js
@@ -1,20 +1,13 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
   testDir: './tests',
+  
   /* Run tests in files in parallel */
+  timeout:40*1000,  
+  // For timeout and load the browser
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -31,12 +24,14 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless:false,//npx playwright test --headed garnu parddaina
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
+       
       use: { ...devices['Desktop Chrome'] },
     },
 
@@ -50,32 +45,9 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+
 });
 
--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,16 +1,20 @@
-const  {test} = require('@playwright/test');
+const  {test, expect} = require('@playwright/test');
 
 
 test('First Playwright Test',  async ({browser}) => { 
     //browser is coming from the test function, we can use it to create a new 
     // context and page
     const context = await browser.newContext(); //like incognito mode 
-    const page = context.newPage();
+    const page = await context.newPage();
     await page.goto('https://www.google.com/');
 });
 
-test('Second Playwright Test',  async ({page}) => { 
+test.only('Second Playwright Test',  async ({page}) => { 
     //page is coming from the test function, we can use it directly
     //  to navigate to the url
     await page.goto('https://www.google.com/');
+
+    const title = await page.title();
+    console.log(title);
+    await expect(page).toHaveTitle('Google');
 });
