3f136ee67ebe875fbb3d3ea74e617ae4ed643391
I am Roshik <Roshik9841@gmail.com>
2026-05-11 17:45:07 +0545

    Page and browser props

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,6 +1,16 @@
 const  {test} = require('@playwright/test');
 
 
-test('First Playwright Test',  async ({browser}) => {
+test('First Playwright Test',  async ({browser}) => { 
+    //browser is coming from the test function, we can use it to create a new 
+    // context and page
+    const context = await browser.newContext(); //like incognito mode 
+    const page = context.newPage();
+    await page.goto('https://www.google.com/');
+});
 
+test('Second Playwright Test',  async ({page}) => { 
+    //page is coming from the test function, we can use it directly
+    //  to navigate to the url
+    await page.goto('https://www.google.com/');
 });
