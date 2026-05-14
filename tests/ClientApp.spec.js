d9b54112eee9874c2310b878be6983b0d92f7cb4
I am Roshik <Roshik9841@gmail.com>
2026-05-14 20:16:23 +0545

    Cart open and load

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,40 +1,44 @@
-const  {test, expect} = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test.only('Login',async({page})=>{
-    const username = page.locator("#userEmail");
-    const password = page.locator("#userPassword");
+test.only("Login", async ({ page }) => {
+  const username = page.locator("#userEmail");
+  const password = page.locator("#userPassword");
 
-    const btn = page.locator("[value='Login']");
-    
-    
-   const products= page.locator(".card-body b"); 
-   const productName = 'Zara Coat 3';
-    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+  const btn = page.locator("[value='Login']");
 
-    const title = await page.title();
-    console.log(title);
+  const products = page.locator(".card-body");
+  const productName = "ZARA COAT 3";
 
-    await username.fill("roshik9841@gmail.com");
-    await password.fill("Roshik9841@!");
-    await btn.click();
+  const cart = page.locator("[routerlink*='cart']");
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
 
+  const title = await page.title();
+  console.log(title);
 
-    await page.waitForLoadState('networkidle'); 
-    // to wait for the page to load completely, it will wait for all the network requests to be finished
+  await username.fill("roshik9841@gmail.com");
+  await password.fill("Roshik9841@!");
+  await btn.click();
 
-    await page.locator(".card-body b").first().waitFor();
-    // to wait for the element to be visible, it will wait for the element to be visible before performing any action on it
-    // , otherwise it will throw an error
+  await page.waitForLoadState("networkidle");
+  // to wait for the page to load completely, it will wait for all the network requests to be finished
 
-    console.log(await page.locator(".card-body b").allTextContents()); 
+  await page.locator(".card-body b").first().waitFor();
+  // to wait for the element to be visible, it will wait for the element to be visible before performing any action on it
+  // , otherwise it will throw an error
 
-    const count = await products.count()
-    for(let i =0;i<count;i++){
-     if(await products.nth(i).locator("b").textContent() === productName){
-        await products.nth(i).locator("text = Add To Cart").click();
-        break;
-     }
-   }
+  console.log(await page.locator(".card-body b").allTextContents());
 
-  
+  const count = await products.count();
+  for (let i = 0; i < count; i++) {
+    if ((await products.nth(i).locator("b").textContent()) === productName) {
+      await products.nth(i).locator("text = Add To Cart").click();
+      break;
+    }
+  }
+  await cart.click();
+  await page.locator("div li").first().waitFor();
+  const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+  // to check if the product is added to the cart, it will check if the product name is visible in the cart page, 
+  // it returns a boolean value
+ expect(bool).toBeTruthy();
 });
