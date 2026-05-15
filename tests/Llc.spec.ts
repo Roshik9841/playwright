568a7f6396550bafb5e1d7b6340c231fce2b0619
I am Roshik <Roshik9841@gmail.com>
2026-05-16 00:10:01 +0545

    Different type of getting the elements

--- tests/Llc.spec.ts
+++ tests/Llc.spec.ts
@@ -7,6 +7,16 @@ test.only("Playwright Special locators",async({page})=>{
 
     await page.getByLabel("Employed").check();
     await page.getByLabel("Gender").selectOption("Female");
+
+    await page.getByPlaceholder("Password").fill("ABC");
+
+    await page.getByRole("button",{name:'Submit'}).click();
+    //{name:'Submit'} to filter among all the button if there are multiple button
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+
+    await page.getByRole("link",{name:'Shop'}).click();
+
+    await page.locator("app-card").filter({hasText:'Nokia Edge'}).getByRole("button").click();
     
 ;
 })
