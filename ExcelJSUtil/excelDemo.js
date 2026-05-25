050ef6c6c040c3be67a7dde7fd522da9221764a8
I am Roshik <Roshik9841@gmail.com>
2026-05-25 18:13:30 +0545

    update and optimized the excel

--- ExcelJSUtil/excelDemo.js
+++ ExcelJSUtil/excelDemo.js
@@ -1,22 +1,29 @@
-const ExcelJs = require('exceljs');
+const ExcelJs = require("exceljs");
 
-async function excelTest(){
-    let output = {row:-1,col:-1};
-    const workbook = new ExcelJs.Workbook();
-    await   workbook.xlsx.readFile('E:/Downloads/excelDownload.xlsx');
-    const worksheet = workbook.getWorksheet('Sheet1');
-    worksheet.eachRow((row,rowNumber)=>{
-        row.eachCell((cell, colNumber)=>{
-            if(cell.value==='Banana'){
-                output.row = rowNumber;
-                output.col = colNumber;
-            }
-        })
-    })
-    const cells = worksheet.getCell(output.row,output.col); // it prints the value of row 3 and cell 2 
-    cells.value='Apple'; // it changes the value of row 3 and cell 2 to IPhone
-    
-    await workbook.xlsx.writeFile('E:/Downloads/excelDownload.xlsx'); // it saves the changes to the same file
-}     
-excelTest();
+let output = { row: -1, col: -1 };
+async function writeExcelTest(searchText, filePath, replaceText) {
+  const workbook = new ExcelJs.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet("Sheet1");
+  await readExcel(worksheet, searchText);
+  const cells = worksheet.getCell(output.row, output.col); // it prints the value of row 3 and cell 2
+  cells.value = replaceText; // it changes the value of row 3 and cell 2 to IPhone
+    const priceChange = worksheet.getCell(output.row,output.col+2);
+    priceChange.value=350;
 
+  await workbook.xlsx.writeFile(filePath); // it saves the changes to the same file
+}
+
+async function readExcel(worksheet, searchText) {
+  worksheet.eachRow((row, rowNumber) => {
+    row.eachCell((cell, colNumber) => {
+      if (cell.value === searchText) {
+        output.row = rowNumber;
+        output.col = colNumber;
+      }
+    });
+  });
+}
+writeExcelTest("Pineapple", "E:/Downloads/excelDownload.xlsx", "Mango");
+
+//update Mango to 350
