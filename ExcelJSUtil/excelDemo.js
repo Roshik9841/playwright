9d189434f0579de1065c23762bddef70ddc9a935
I am Roshik <Roshik9841@gmail.com>
2026-05-25 18:03:46 +0545

    Write into excel code

--- ExcelJSUtil/excelDemo.js
+++ ExcelJSUtil/excelDemo.js
@@ -1,14 +1,22 @@
 const ExcelJs = require('exceljs');
 
 async function excelTest(){
-
+    let output = {row:-1,col:-1};
     const workbook = new ExcelJs.Workbook();
     await   workbook.xlsx.readFile('E:/Downloads/excelDownload.xlsx');
     const worksheet = workbook.getWorksheet('Sheet1');
     worksheet.eachRow((row,rowNumber)=>{
         row.eachCell((cell, colNumber)=>{
-            console.log(`Row ${rowNumber} Cell ${colNumber} = ${cell.value}`);
+            if(cell.value==='Banana'){
+                output.row = rowNumber;
+                output.col = colNumber;
+            }
         })
     })
-}
+    const cells = worksheet.getCell(output.row,output.col); // it prints the value of row 3 and cell 2 
+    cells.value='Apple'; // it changes the value of row 3 and cell 2 to IPhone
+    
+    await workbook.xlsx.writeFile('E:/Downloads/excelDownload.xlsx'); // it saves the changes to the same file
+}     
 excelTest();
+
